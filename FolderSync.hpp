9133078850c9d9b7447cb95dd4c016fef10fe01c
chenjingzhi <1286576927@qq.com>
2022-09-30 21:37:14 +0800

    Change the criterion for file consistency from a single "file name" to "file name and file size".

--- FolderSync.hpp
+++ FolderSync.hpp
@@ -2,8 +2,6 @@
 // Created by Chenjingzhi on 2022/9/29.
 //
 
-// 注：该程序只能以文件为整体进行更新，如文件的新增和删减，文件内部的更新无法同步 TODO: 增加此功能
-
 #ifndef FOLDERSYNC_HPP
 #define FOLDERSYNC_HPP
 
@@ -11,8 +9,9 @@
 #include <fstream>
 #include <io.h>
 #include <string>
-#include <unordered_set>
+#include <unordered_map>
 #include <utility>
+#include <sys/stat.h>
 
 
 class FolderSync {
@@ -20,8 +19,7 @@ public:
     FolderSync(std::string src_path, std::string dst_path)
             : m_src_path(std::move(src_path)),
               m_dst_path(std::move(dst_path)) {
-        m_src_path_length = (int) m_src_path.length();
-        m_dst_path_length = (int) m_dst_path.length();
+        files_init();
     };
 
     ~FolderSync() = default;
@@ -36,77 +34,91 @@ public:
 
 private:
     std::string m_src_path;
-    int m_src_path_length;
-    std::unordered_set<std::string> m_src_files;
+    std::unordered_map<std::string, long> m_src_files;
 
     std::string m_dst_path;
-    int m_dst_path_length;
-    std::unordered_set<std::string> m_dst_files;
+    std::unordered_map<std::string, long> m_dst_files;
+
+    std::unordered_map<std::string, long> m_new_files;
+    std::unordered_map<std::string, long> m_old_files;
 
 public:
     void update() {
-        // get files list
-        get_files(m_src_path, m_src_files, m_src_path_length);
-        get_files(m_dst_path, m_dst_files, m_dst_path_length);
-
-        // create new files
-        for (const auto &i : m_src_files) {
-            auto iter = m_dst_files.find(i);
-
-            if (iter == m_dst_files.end()) {
-                copy_files(m_src_path + i, m_dst_path + i);
-            } else {  // iter != m_dst_files.end()
-                m_dst_files.erase(iter);
-            }
-        }
+        if (m_new_files.empty() && m_old_files.empty()) find_files_diff();
 
         // delete old files
-        for (const auto &i : m_dst_files) {
-            remove((m_dst_path + i).c_str());
+        for (const auto &i : m_old_files) {
+            remove((m_dst_path + i.first).c_str());
+        }
+
+        // create new files
+        for (const auto &i : m_new_files) {
+            std::string cmd;
+            system(("copy \"" + m_src_path + i.first + "\" \"" + m_dst_path + i.first + "\"").c_str());
         }
     }
 
+    void find_files_diff() {
+        if (m_src_files.empty()) {
+            std::cout << "Error: source files empty!" << std::endl;
+            return;
+        }
+
+        m_old_files.insert(m_dst_files.begin(), m_dst_files.end());
+
+        // get new files
+        for (const auto &i : m_src_files) {
+            auto iter = m_old_files.find(i.first);
+            if (iter != m_old_files.end() && i.second == iter->second) {  // already exist
+                m_old_files.erase(iter);
+            } else {  // not exist
+                m_new_files.insert(i);
+            }
+        }
+
+        // print new and old files
+        std::cout << "new files :" << std::endl;
+        for (const auto &i : m_new_files) std::cout << i.first << ", " << i.second << std::endl;
+        std::cout << "old files :" << std::endl;
+        for (const auto &i : m_old_files) std::cout << i.first << ", " << i.second << std::endl;
+    }
+
+    void find_empty_sub_folder() {}  // TODO
+
 private:
-    static void get_files(const std::string &path, std::unordered_set<std::string> &files, int base_path_length) {
-        long long int file_handle;  // 文件句柄
+    void files_init() {
+        if (m_src_path == m_dst_path) {
+            std::cout << "Error: The source and destination addresses must be different!" << std::endl;
+            return;
+        }
+
+        // get files list
+        get_files(m_src_path, m_src_files, (int) m_src_path.length());
+        get_files(m_dst_path, m_dst_files, (int) m_dst_path.length());
+    }
+
+    static
+    void get_files(const std::string &path, std::unordered_map<std::string, long> &files, int root_path_length) {
+        long file_handle;  // 文件句柄
         struct _finddata_t file_info{};  // 文件信息
+        struct _stat stat_info{};  // 文件状态信息
 
         std::string p;
         if ((file_handle = _findfirst(p.assign(path).append("\\*").c_str(), &file_info)) != -1) {
             do {
                 if ((file_info.attrib & _A_SUBDIR)) {  // 表示文件夹
                     if (strcmp(file_info.name, ".") != 0 && strcmp(file_info.name, "..") != 0) {
-                        get_files(p.assign(path).append("\\").append(file_info.name), files, base_path_length);  //递归搜索
+                        p.assign(path).append("\\").append(file_info.name);
+                        get_files(p, files, root_path_length);  // 递归搜索
                     }
                 } else {  // (file_info.attrib & _A_SUBDIR) == 0，表示文件
                     p.assign(path).append("\\").append(file_info.name);
-                    files.emplace(p.substr(base_path_length, p.length() - base_path_length));
+                    _stat(p.c_str(), &stat_info);
+                    files.emplace(p.substr(root_path_length, p.length() - root_path_length), stat_info.st_size);
                 }
             } while (_findnext(file_handle, &file_info) == 0);  // 处理下一个，存在则返回 0，否则返回 -1
             _findclose(file_handle);
         }
-    }
-
-    static void copy_files(const std::string &src_path, const std::string &dst_path) {
-        std::ifstream in;
-        in.open(src_path);
-        if (!in) {
-            std::cout << "Error: open src file < " << src_path << " > failed!" << std::endl;
-            return;
-        }
-
-        std::ofstream out;
-        out.open(dst_path);
-        if (!out) {
-            std::cout << "Error: create dst file < " << dst_path << " > failed!" << std::endl;
-            in.close();
-            return;
-        }
-
-        out << in.rdbuf();
-
-        out.close();
-        in.close();
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@ using namespace std;
 
 
 int main() {
-    string src_path = R"(C:\Users\12865\Desktop\encrypt_001)";
-    string dst_path = R"(C:\Users\12865\Desktop\encrypt_002)";
+    // TODO: GUI
+
+    // 千万注意不要写反了！！！dst path 中的文件可能会被删除！！！
+    string src_path = R"(a)";
+    string dst_path = R"(b)";
 
     FolderSync folder_sync(src_path, dst_path);
-    folder_sync.update();
+    folder_sync.find_files_diff();
 
     return 0;
 }
